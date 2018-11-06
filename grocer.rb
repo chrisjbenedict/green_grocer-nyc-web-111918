726220ad06b11390842d304c1b63ad8d0e342544
Christopher Benedict <chrisjbenedict@gmail.com>
2018-11-06 18:38:48 +0000

    automatically backed up by learn

--- grocer.rb
+++ grocer.rb
@@ -1,9 +1,27 @@
 def consolidate_cart(cart)
   # code here
+  # .each_with_object iterates the given block for each element with an 
+  # arbitrary object given, and returns the initially given object.
+  cart.each_with_object({}) do |item, hash|
+    # over each element/object pair, iterate over the food type/attribute pair
+    item.each do |type, attributes|
+      # if type already exists
+      if hash[type]
+        # increment count by 1
+        attributes[:count] += 1
+      else
+        # because type doesn't already exist, create a count attribute equal to 1 item
+        attributes[:count] = 1
+        # and update attributes
+        hash[type] = attributes
+      end
+    end
+  end
 end
 
 def apply_coupons(cart, coupons)
   # code here
+  
 end
 
 def apply_clearance(cart)
